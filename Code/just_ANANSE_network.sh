a33a532dd364dcdb26b43570b2474152566e7494
Joel Pablos Martín <joelpablos.ed@gmail.com>
2023-08-11 19:38:58 +0200

    Delete just_ANANSE_network.sh

--- Code/just_ANANSE_network.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ananse network  ../Results/ANANSE_binding/binding.h5 \
-                 -e ../Data/ATLAS/MouseStSt/KC_pseudobulk.tsv \
-                 -c tpm
-                 -g ../Data/Genomes/mm39 \
-                 -o ../Results/ANANSE_network/ANANSE_network.tsv
-
-echo "ANANSE network has been created successfully!!"
